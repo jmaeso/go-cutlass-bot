565366af064853ab7af87b08aad72efb06f0a616
jmaeso <jmaeso@enti.cat>
2017-11-01 00:53:44 +0100

    Add simplest bot with tgbotapi

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/jmaeso/go-cutlass-bot/app"
 	"github.com/jmaeso/go-cutlass-bot/tools/yaml"
+	"gopkg.in/telegram-bot-api.v4"
 )
 
 func main() {
@@ -19,5 +19,30 @@ func main() {
 		log.Fatalf("Token required in settings.yml")
 	}
 
-	fmt.Printf("Token: %s\n", token)
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
